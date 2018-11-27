a9a3a5247fc9ee168de7a3d1e8f4c20f81da0221
Takashi Sugimoto <takashi.sugimoto.git@gmail.com>
2018-11-27 12:53:10 +0900

    fix: /edit ページが閲覧できないバグを修正
    ページ側でhistoryをchangeしており、そのときにページ更新で404 NotFoundになっていた
    存在しないpathへRequestした場合 index.html を返却するように変更

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 
 app.use(express.static(__dirname + '/build'));
+app.use((req, res) => {
+    res.sendFile(__dirname + '/build/index.html');
+});
 
 app.listen(3000, '0.0.0.0', () => {
     console.info('server started.');
